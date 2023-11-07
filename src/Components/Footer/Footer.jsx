c5c974619e837958d196558731ed045999183b88
jayesh-mali607 <jayeshmali607@gmail.com>
2023-11-07 16:05:12 +0530

    chnages in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,6 +9,13 @@ const Footer = () => {
             &copy; 2023 Your Clothing Brand
           </div>
         </div>
+        {/* for remove  */}
+
+        <div className="container">
+          <div className="footer-content" id="footer-content">
+            &copy; 2023 Your Clothing Brand
+          </div>
+        </div>
       </footer>
       ;
     </>
