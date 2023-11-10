9ec99571d1306aeba6622727b6ea04915b06947c
jayesh-mali607 <jayeshmali607@gmail.com>
2023-11-10 16:15:00 +0530

    remove some code in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,13 +9,7 @@ const Footer = () => {
             &copy; 2023 Your Clothing Brand
           </div>
         </div>
-        {/* for remove  */}
 
-        <div className="container">
-          <div className="footer-content" id="footer-content">
-            &copy; 2023 Your Clothing Brand
-          </div>
-        </div>
       </footer>
       ;
     </>
